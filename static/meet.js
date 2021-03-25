192b122f9a929788aad9f661b1d26e1de227c25c
Nathan <suprdory@google.com>
2021-03-25 00:35:56 +0000

    add username+mode edit option

--- static/meet.js
+++ static/meet.js
@@ -69,7 +69,10 @@ function populateMeetTable(data){
   mplon.innerHTML = parseFloat(data.midpoint_lon).toFixed(4);
   mplat.innerHTML = parseFloat(data.midpoint_lat).toFixed(4);
 }
-
+// function selectElement(id, valueToSelect) {
+//     let element = document.getElementById(id);
+//     element.value = valueToSelect;
+// }
 function populateUserTable(data){
   console.log(data);
   var table = document.getElementById("userTable");
@@ -77,20 +80,66 @@ function populateUserTable(data){
     var row = table.insertRow(-1);
     var useridtab = row.insertCell(0);
     var usernametab = row.insertCell(-1);
+
+    let uNameInput = document.createElement("input")
+    uNameInput.className = 'mapbutton'
+    uNameInput.value = d.username
+    uNameInput.id = "username" + d.userid;
+    usernametab.appendChild(uNameInput)
+
     var ulon = row.insertCell(-1);
     var ulat = row.insertCell(-1);
     var udur = row.insertCell(-1);
     var udist = row.insertCell(-1);
+
     var gMode = row.insertCell(-1);
+    let gModeInp = document.createElement("select")
+    gModeInp.className = 'mapbutton'
+    gModeInp.id="gMode"+d.userid
+
+    var option =  document.createElement("option");
+    option.text = "Walking"
+    option.value = "WALKING"
+    gModeInp.add(option)
+
+    var option =  document.createElement("option");
+    option.text = "Cycling"
+    option.value = "BICYCLING"
+    gModeInp.add(option)
+
+    var option =  document.createElement("option");
+    option.text = "Public Transport"
+    option.value = "TRANSIT"
+    gModeInp.add(option)
+
+    var option =  document.createElement("option");
+    option.text = "Driving"
+    option.value = "DRIVING"
+    gModeInp.add(option)
+
+    console.log(d.gRouteMode);
+    // gModeInp.value=d.gRouteMode
+    gMode.appendChild(gModeInp)
+    document.getElementById("gMode"+d.userid).value =d.gRouteMode
+
+    var uUpdate = row.insertCell(-1);
     var udel = row.insertCell(-1);
 
     useridtab.innerHTML = d.userid;
-    usernametab.innerHTML = d.username;
+    // usernametab.innerHTML = d.username;
     ulon.innerHTML = parseFloat(d.lon).toFixed(4);
     ulat.innerHTML = parseFloat(d.lat).toFixed(4);
     udur.innerHTML =d.gRouteDuration;
     udist.innerHTML = d.gRouteDistance;
-    gMode.innerHTML = d.gRouteMode;
+    // gMode.innerHTML = d.gRouteMode;
+
+    let ubtn = document.createElement("a");
+    ubtn.className = 'mapbutton'
+    ubtn.textContent = "Update"
+    ubtn.id = "btnUpdate" + d.userid;
+    // btn.addEventListener("click",deleteuser.bind(this,d.userid));
+    ubtn.addEventListener("click",function(){updateUser(d.userid)});
+    udel.appendChild(ubtn)
 
     let btn = document.createElement("a");
     btn.className = 'mapbutton'
@@ -99,7 +148,27 @@ function populateUserTable(data){
     // btn.addEventListener("click",deleteuser.bind(this,d.userid));
     btn.addEventListener("click",function(){deleteuser(d.userid)});
     udel.appendChild(btn)
+
+
   }
+}
+
+function updateUser(userid) {
+
+  patchdata={
+    username:$("username"+userid).value,
+    gRouteMode:$("gMode"+userid).value,
+  };
+  console.log(patchdata)
+  url='/user/'+userid.toString()+'?meetkey='+meetkey;
+  fetch(baseUrl+url,{
+    method:'PATCH',
+    body:JSON.stringify(patchdata),
+    headers:{'Content-Type': 'application/json; charset=utf-8'}})
+  .then(data=>{return data.json()})
+  .then(res=>{console.log(res)})
+  // .then(error=>{console.log(error)})
+  // window.location.href = "/web/meet/" + meet;
 }
 
 getMeet(meetid);
