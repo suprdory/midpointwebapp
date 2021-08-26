6742ed1fb4099252811898cddd7b373b1655155a
Nathan <suprdory@google.com>
2021-08-26 19:33:12 +0100

    try heroku

--- static/globalvars.js
+++ static/globalvars.js
@@ -3,4 +3,5 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 // const baseUrl = 'http://localhost:5000/'
-const baseUrl = 'https://suprdory.eu.pythonanywhere.com/'
+// const baseUrl = 'https://suprdory.eu.pythonanywhere.com/'
+const baseUrl = 'https://midpointapi.herokuapp.com/'
